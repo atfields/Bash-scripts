a99e1efc3579d396def54fbbd52bcd7e28b1c6a2
atfields <dufields@gmail.com>
2018-10-24 16:06:31 -0500

    Update charts.sh
    The heterozygosity of loci added to this script

--- ddRAD scripts/charts.sh
+++ ddRAD scripts/charts.sh
@@ -28,6 +28,7 @@ vcftools --vcf ../$1 --out out --relatedness
 vcftools --vcf ../$1 --out out --site-depth
 vcftools --vcf ../$1 --out out --singletons
 vcftools --vcf ../$1 --out out --geno-depth
+vcftools --vcf ../$1 --out out --hardy
 vcftools --vcf ../$1 --out opt --012
 cut -f 1 out.imiss > tmp
 tail -n +2 tmp | sed -r 's/([0-9]+)_/\1\t/g' | sed -r 's/([0-9]+)./\1\t/g' > tmp2
@@ -35,6 +36,7 @@ sed -i '1s/^/Sample\tLib\tIndex\tRow\n/' tmp2
 paste tmp tmp2 > out.samples
 sed -i '1s/^/CHROM POS N_ALLELES N_CHR FREQ1 FREQ2 FREQ3 FREQ4 FREQ5\n/' out.frq
 #echo "CHROM POS N_ALLELES N_CHR FREQ1 FREQ2 FREQ3 FREQ4 FREQ5" > out.fin.frq & echo "$(tail -n +2 out.frq)" >> out.fin.frq
+awk -F'[/\t]' '{OFS="\t"} NR==1 {print "CHROM",$2,$10,$11,$12, "N", "PER_HET"} NR>1 {print $1,$2,$10,$11,$12,$3+$4+$5,$4/($3+$4+$5)}' out.hwe > out.hwe.alter
 cp ../popmap .
 cp ~/bin/plotting.r .
 cp ~/bin/opt.script .
